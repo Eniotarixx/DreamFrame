8842f4e005962f043d04a79909413506ff20f7ee
Eniot <sunather25@gmail.com>
2025-07-03 17:07:32 +0900

    camera resolution set to the one supported by the model

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@
 const video = document.getElementById('video');
 const generateBtn = document.getElementById('Generate');
 
-navigator.mediaDevices.getUserMedia({ video: true }) // ask the navigator to acces to the webcam
+navigator.mediaDevices.getUserMedia({ video: { width: { ideal: 512 }, height: { ideal: 512 } } }) // ask the navigator to acces to the webcam with a resolution of 512x512 if possible > ideal 
     .then(stream => { // if we have the webcam 
         video.srcObject = stream; // tell balise video to show this stream  
     })
